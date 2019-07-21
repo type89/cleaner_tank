c3f015cc7df3853ff24567dec09dbadbd9a1ec8d
type89 <zong1yuan2@gmail.com>
2019-07-21 16:36:34 +0900

    フレーム構造に変更

--- tank_controller_class_frame.py
+++ tank_controller_class_frame.py
@@ -5,13 +5,13 @@ import serial
 L_duty_array = {"-4":"150","-3":"145", "-2":"105", "-1":"080","0":"000", "1":"080", "2":"105","3":"145", "4":"150"}
 R_duty_array = {"-4":"115","-3":"110", "-2":"080", "-1":"060","0":"000", "1":"060", "2":"080","3":"110", "4":"115"}
 
-ser = serial.Serial('/dev/rfcomm9', 9600)
+ser = serial.Serial('/dev/rfcomm11', 9600)
 
 class Application(tk.Frame):
     def __init__(self,master,left_duty='0',right_duty='0'):
         super().__init__(master)
         #self.pack()
-        master.geometry("600x400")
+        master.geometry("300x200")
         master.title("Tank Controller")
         master.bind('<Up>', self.forward)
         master.bind('<space>', self.stop)
@@ -23,25 +23,32 @@ class Application(tk.Frame):
         self.create_widgets(master,left_duty,right_duty)
 
     def create_widgets(self,master,left_duty,right_duty):
-        self.left_duty = tk.Label(master, text=left_duty, relief=tk.FLAT, bd=2,background="white")
-        self.right_duty = tk.Label(master, text=right_duty, relief=tk.FLAT, bd=2,background="white")
-        forward = tk.Button(master, text="↑", fg="blue", command=self.forward)
-        backward = tk.Button(master, text="↓", fg="blue", command=self.backward)
-        right = tk.Button(master, text="→", fg="green", command=self.right)
-        left = tk.Button(master, text="←", fg="green", command=self.left)
-        RR = tk.Button(master, text=">>", fg="purple", command=self.RR)
-        LL = tk.Button(master, text="<<", fg="purple", command=self.LL)
-        stop = tk.Button(master, text="STOP", fg="red", command=self.stop)
-
-        self.left_duty.grid(row=0, column=0, columnspan=2, sticky="we")
-        self.right_duty.grid(row=0, column=3, columnspan=2, sticky="we")
-        forward.grid(row=1, column=1, columnspan=3, sticky="we")
-        LL.grid(row=2, column=0)
-        left.grid(row=2, column=1)
-        stop.grid(row=2, column=2)
-        right.grid(row=2, column=3)
-        RR.grid(row=2, column=4)
-        backward.grid(row=3, column=1, columnspan=3, sticky="we")
+        frame1 = tk.Frame(master)
+        self.left_label_area = tk.Label(frame1, text='L-Motor', relief=tk.FLAT, bd=2)
+        self.right_label_area = tk.Label(frame1, text='R-Motor', relief=tk.FLAT, bd=2)
+        self.left_duty = tk.Label(frame1, text=left_duty, relief=tk.FLAT, bd=2,background="white")
+        self.right_duty = tk.Label(frame1, text=right_duty, relief=tk.FLAT, bd=2,background="white")
+        self.space_area = tk.Label(frame1, text='', relief=tk.FLAT, bd=2)
+        forward = tk.Button(frame1, text="↑", fg="blue", command=self.forward)
+        backward = tk.Button(frame1, text="↓", fg="blue", command=self.backward)
+        right = tk.Button(frame1, text="→", fg="green", command=self.right)
+        left = tk.Button(frame1, text="←", fg="green", command=self.left)
+        RR = tk.Button(frame1, text=">>", fg="purple", command=self.RR)
+        LL = tk.Button(frame1, text="<<", fg="purple", command=self.LL)
+        stop = tk.Button(frame1, text="STOP", fg="red", command=self.stop)
+        self.left_label_area.grid(row=0, column=0, columnspan=2, sticky="we")
+        self.right_label_area.grid(row=0, column=3, columnspan=2, sticky="we")
+        self.left_duty.grid(row=1, column=0, columnspan=2, sticky="we")
+        self.right_duty.grid(row=1, column=3, columnspan=2, sticky="we")
+        self.space_area.grid(row=2, column=0, columnspan=5, sticky="we")
+        forward.grid(row=3, column=1, columnspan=3, sticky="we")
+        LL.grid(row=4, column=0)
+        left.grid(row=4, column=1)
+        stop.grid(row=4, column=2)
+        right.grid(row=4, column=3)
+        RR.grid(row=4, column=4)
+        backward.grid(row=5, column=1, columnspan=3, sticky="we")
+        frame1.pack(padx=5, pady=5)
 
     def send_command(self,command):
         arg = str(command) + ";"
@@ -161,8 +168,8 @@ class Application(tk.Frame):
         return
 
 def main():
-    win = tk.Tk()
-    app = Application(master=win)
+    root = tk.Tk()
+    app = Application(master=root)
     app.mainloop()
 
 if __name__ == "__main__":
